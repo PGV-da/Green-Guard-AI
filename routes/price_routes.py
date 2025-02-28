d523a07cb1cf5bd74e0d82dc0a3f9568558ba163
Vikram <vikramji283@gmail.com>
2025-02-28 20:29:15 +0530

    price prediction ML update

--- routes/price_routes.py
+++ routes/price_routes.py
@@ -1,49 +1,338 @@
+import random
 import pandas as pd
+import numpy as np
+from datetime import datetime
 from flask import Blueprint, jsonify, render_template, request
-from statsmodels.tsa.arima.model import ARIMA
 
 from utils.authentication import require_login
+from services import crops
 
 price_bp = Blueprint('price', __name__)
 
-@price_bp.route('/price')
+commodity_dict = {
+    "arhar": "data/crop_data/Arhar.csv",
+    "bajra": "data/crop_data/Bajra.csv",
+    "barley": "data/crop_data/Barley.csv",
+    "copra": "data/crop_data/Copra.csv",
+    "cotton": "data/crop_data/Cotton.csv",
+    "sesamum": "data/crop_data/Sesamum.csv",
+    "gram": "data/crop_data/Gram.csv",
+    "groundnut": "data/crop_data/Groundnut.csv",
+    "jowar": "data/crop_data/Jowar.csv",
+    "maize": "data/crop_data/Maize.csv",
+    "masoor": "data/crop_data/Masoor.csv",
+    "moong": "data/crop_data/Moong.csv",
+    "niger": "data/crop_data/Niger.csv",
+    "paddy": "data/crop_data/Paddy.csv",
+    "ragi": "data/crop_data/Ragi.csv",
+    "rape": "data/crop_data/Rape.csv",
+    "jute": "data/crop_data/Jute.csv",
+    "safflower": "data/crop_data/Safflower.csv",
+    "soyabean": "data/crop_data/Soyabean.csv",
+    "sugarcane": "data/crop_data/Sugarcane.csv",
+    "sunflower": "data/crop_data/Sunflower.csv",
+    "urad": "data/crop_data/Urad.csv",
+    "wheat": "data/crop_data/Wheat.csv"
+}
+
+annual_rainfall = [29, 21, 37.5, 30.7, 52.6, 150, 299, 251.7, 179.2, 70.5, 39.8, 10.9]
+
+base = {
+    "Paddy": 1245.5,
+    "Arhar": 3200,
+    "Bajra": 1175,
+    "Barley": 980,
+    "Copra": 5100,
+    "Cotton": 3600,
+    "Sesamum": 4200,
+    "Gram": 2800,
+    "Groundnut": 3700,
+    "Jowar": 1520,
+    "Maize": 1175,
+    "Masoor": 2800,
+    "Moong": 3500,
+    "Niger": 3500,
+    "Ragi": 1500,
+    "Rape": 2500,
+    "Jute": 1675,
+    "Safflower": 2500,
+    "Soyabean": 2200,
+    "Sugarcane": 2250,
+    "Sunflower": 3700,
+    "Urad": 4300,
+    "Wheat": 1350
+
+}
+commodity_list = []
+
+class Commodity:
+
+    def __init__(self, csv_name):
+        self.name = csv_name
+        dataset = pd.read_csv(csv_name)
+        self.X = dataset.iloc[:, :-1].values
+        self.Y = dataset.iloc[:, 3].values
+
+        #from sklearn.model_selection import train_test_split
+        #X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=0)
+
+        # Fitting decision tree regression to dataset
+        from sklearn.tree import DecisionTreeRegressor
+        depth = random.randrange(7,18)
+        self.regressor = DecisionTreeRegressor(max_depth=depth)
+        self.regressor.fit(self.X, self.Y)
+        #y_pred_tree = self.regressor.predict(X_test)
+        # fsa=np.array([float(1),2019,45]).reshape(1,3)
+        # fask=regressor_tree.predict(fsa)
+
+    def getPredictedValue(self, value):
+        if value[1]>=2019:
+            fsa = np.array(value).reshape(1, 3)
+            #print(" ",self.regressor.predict(fsa)[0])
+            return self.regressor.predict(fsa)[0]
+        else:
+            c=self.X[:,0:2]
+            x=[]
+            for i in c:
+                x.append(i.tolist())
+            fsa = [value[0], value[1]]
+            ind = 0
+            for i in range(0,len(x)):
+                if x[i]==fsa:
+                    ind=i
+                    break
+            #print(index, " ",ind)
+            #print(x[ind])
+            #print(self.Y[i])
+            return self.Y[ind]
+
+    def getCropName(self):
+        a = self.name.split('.')
+        return a[0]
+
+# Load Commodities
+for crop, path in commodity_dict.items():
+    commodity_list.append(Commodity(path))
+
+def TwelveMonthsForecast(name):
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    current_rainfall = annual_rainfall[current_month - 1]
+    name = name.lower()
+    commodity = commodity_list[0]
+    for i in commodity_list:
+        if name == str(i):
+            commodity = i
+            break
+    month_with_year = []
+    for i in range(1, 13):
+        if current_month + i <= 12:
+            month_with_year.append((current_month + i, current_year, annual_rainfall[current_month + i - 1]))
+        else:
+            month_with_year.append((current_month + i - 12, current_year + 1, annual_rainfall[current_month + i - 13]))
+    max_index = 0
+    min_index = 0
+    max_value = 0
+    min_value = 9999
+    wpis = []
+    current_wpi = commodity.getPredictedValue([float(current_month), current_year, current_rainfall])
+    change = []
+
+    for m, y, r in month_with_year:
+        current_predict = commodity.getPredictedValue([float(m), y, r])
+        if current_predict > max_value:
+            max_value = current_predict
+            max_index = month_with_year.index((m, y, r))
+        if current_predict < min_value:
+            min_value = current_predict
+            min_index = month_with_year.index((m, y, r))
+        wpis.append(current_predict)
+        change.append(((current_predict - current_wpi) * 100) / current_wpi)
+
+    max_month, max_year, r1 = month_with_year[max_index]
+    min_month, min_year, r2 = month_with_year[min_index]
+    min_value = min_value * base[name.capitalize()] / 100
+    max_value = max_value * base[name.capitalize()] / 100
+    crop_price = []
+    for i in range(0, len(wpis)):
+        m, y, r = month_with_year[i]
+        x = datetime(y, m, 1)
+        x = x.strftime("%b %y")
+        crop_price.append([x, round((wpis[i]* base[name.capitalize()]) / 100, 2) , round(change[i], 2)])
+   # print("forecasr", wpis)
+    x = datetime(max_year,max_month,1)
+    x = x.strftime("%b %y")
+    max_crop = [x, round(max_value,2)]
+    x = datetime(min_year, min_month, 1)
+    x = x.strftime("%b %y")
+    min_crop = [x, round(min_value,2)]
+
+    return max_crop, min_crop, crop_price
+
+def TwelveMonthPrevious(name):
+    name = name.lower()
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    current_rainfall = annual_rainfall[current_month - 1]
+    commodity = commodity_list[0]
+    wpis = []
+    crop_price = []
+    for i in commodity_list:
+        if name == str(i):
+            commodity = i
+            break
+    month_with_year = []
+    for i in range(1, 13):
+        if current_month - i >= 1:
+            month_with_year.append((current_month - i, current_year, annual_rainfall[current_month - i - 1]))
+        else:
+            month_with_year.append((current_month - i + 12, current_year - 1, annual_rainfall[current_month - i + 11]))
+
+    for m, y, r in month_with_year:
+        current_predict = commodity.getPredictedValue([float(m), 2013, r])
+        wpis.append(current_predict)
+
+    for i in range(0, len(wpis)):
+        m, y, r = month_with_year[i]
+        x = datetime(y,m,1)
+        x = x.strftime("%b %y")
+        crop_price.append([x, round((wpis[i]* base[name.capitalize()]) / 100, 2)])
+   # print("previous ", wpis)
+    new_crop_price =[]
+    for i in range(len(crop_price)-1,-1,-1):
+        new_crop_price.append(crop_price[i])
+    return new_crop_price
+
+def CurrentMonth(name):
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    current_rainfall = annual_rainfall[current_month - 1]
+    name = name.lower()
+    commodity = commodity_list[0]
+    for i in commodity_list:
+        if name == str(i):
+            commodity = i
+            break
+    current_wpi = commodity.getPredictedValue([float(current_month), current_year, current_rainfall])
+    current_price = (base[name.capitalize()]*current_wpi)/100
+    return current_price
+
+def SixMonthsForecastHelper(name):
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    current_rainfall = annual_rainfall[current_month - 1]
+    name = name.split("/")[1]
+    name = name.lower()
+    commodity = commodity_list[0]
+    for i in commodity_list:
+        if name == str(i):
+            commodity = i
+            break
+    month_with_year = []
+    for i in range(1, 7):
+        if current_month + i <= 12:
+            month_with_year.append((current_month + i, current_year, annual_rainfall[current_month + i - 1]))
+        else:
+            month_with_year.append((current_month + i - 12, current_year + 1, annual_rainfall[current_month + i - 13]))
+    wpis = []
+    current_wpi = commodity.getPredictedValue([float(current_month), current_year, current_rainfall])
+    change = []
+
+    for m, y, r in month_with_year:
+        current_predict = commodity.getPredictedValue([float(m), y, r])
+        wpis.append(current_predict)
+        change.append(((current_predict - current_wpi) * 100) / current_wpi)
+
+    crop_price = []
+    for i in range(0, len(wpis)):
+        m, y, r = month_with_year[i]
+        x = datetime(y, m, 1)
+        x = x.strftime("%b %y")
+        crop_price.append([x, round((wpis[i]* base[name.capitalize()]) / 100, 2) , round(change[i], 2)])
+
+   # print("Crop_Price: ", crop_price)
+    return crop_price
+
+def SixMonthsForecast():
+    month1=[]
+    month2=[]
+    month3=[]
+    month4=[]
+    month5=[]
+    month6=[]
+    for i in commodity_list:
+        crop=SixMonthsForecastHelper(i.getCropName())
+        k=0
+        for j in crop:
+            time = j[0]
+            price = j[1]
+            change = j[2]
+            if k==0:
+                month1.append((price,change,i.getCropName().split("/")[1],time))
+            elif k==1:
+                month2.append((price,change,i.getCropName().split("/")[1],time))
+            elif k==2:
+                month3.append((price,change,i.getCropName().split("/")[1],time))
+            elif k==3:
+                month4.append((price,change,i.getCropName().split("/")[1],time))
+            elif k==4:
+                month5.append((price,change,i.getCropName().split("/")[1],time))
+            elif k==5:
+                month6.append((price,change,i.getCropName().split("/")[1],time))
+            k+=1
+    month1.sort()
+    month2.sort()
+    month3.sort()
+    month4.sort()
+    month5.sort()
+    month6.sort()
+    crop_month_wise=[]
+    crop_month_wise.append([month1[0][3],month1[len(month1)-1][2],month1[len(month1)-1][0],month1[len(month1)-1][1],month1[0][2],month1[0][0],month1[0][1]])
+    crop_month_wise.append([month2[0][3],month2[len(month2)-1][2],month2[len(month2)-1][0],month2[len(month2)-1][1],month2[0][2],month2[0][0],month2[0][1]])
+    crop_month_wise.append([month3[0][3],month3[len(month3)-1][2],month3[len(month3)-1][0],month3[len(month3)-1][1],month3[0][2],month3[0][0],month3[0][1]])
+    crop_month_wise.append([month4[0][3],month4[len(month4)-1][2],month4[len(month4)-1][0],month4[len(month4)-1][1],month4[0][2],month4[0][0],month4[0][1]])
+    crop_month_wise.append([month5[0][3],month5[len(month5)-1][2],month5[len(month5)-1][0],month5[len(month5)-1][1],month5[0][2],month5[0][0],month5[0][1]])
+    crop_month_wise.append([month6[0][3],month6[len(month6)-1][2],month6[len(month6)-1][0],month6[len(month6)-1][1],month6[0][2],month6[0][0],month6[0][1]])
+
+   # print(crop_month_wise)
+    return crop_month_wise
+
+@price_bp.route('/commodity/<name>')
 @require_login
-def price():
-    return render_template('price.html')
+def crop_profile(name):
+    max_crop, min_crop, forecast_crop_values = TwelveMonthsForecast(name)
+    prev_crop_values = TwelveMonthPrevious(name)
+    forecast_x = [i[0] for i in forecast_crop_values]
+    forecast_y = [i[1] for i in forecast_crop_values]
+    previous_x = [i[0] for i in prev_crop_values]
+    previous_y = [i[1] for i in prev_crop_values]
+    current_price = CurrentMonth(name)
+    #print(max_crop)
+    #print(min_crop)
+    #print(forecast_crop_values)
+    #print(prev_crop_values)
+    #print(str(forecast_x))
+    crop_data = crops.crop(name)
+    context = {
+        "name":name,
+        "max_crop": max_crop,
+        "min_crop": min_crop,
+        "forecast_values": forecast_crop_values,
+        "forecast_x": str(forecast_x),
+        "forecast_y":forecast_y,
+        "previous_values": prev_crop_values,
+        "previous_x":previous_x,
+        "previous_y":previous_y,
+        "current_price": current_price,
+        "image_url":crop_data[0],
+        "prime_loc":crop_data[1],
+        "type_c":crop_data[2],
+        "export":crop_data[3]
+    }
+    return render_template('price-commodity.html', context=context)
 
 @price_bp.route('/priceprediction')
 @require_login
 def price_prediction():
     """Renders the price prediction page."""
     return render_template('price-prediction.html')
-
-@price_bp.route('/forecast', methods=['GET'])
-@require_login
-def forecast():
-    """Forecasts crop prices using an ARIMA model."""
-    selected_crop = request.args.get('crop')
-
-    if not selected_crop:
-        return jsonify({'error': 'No crop selected'}), 400
-
-    try:
-        # Load crop price data
-        data = pd.read_csv(f'data/{selected_crop}.csv', header=None, names=['Date', 'Value'])
-        data['Date'] = pd.to_datetime(data['Date'], format='%Y-%m')
-        data.set_index('Date', inplace=True)
-
-        # Fit ARIMA model
-        model = ARIMA(data['Value'], order=(1,1,1))  # Example order (p,d,q) - should be fine-tuned
-        fit_model = model.fit()
-
-        # Forecast for the next 4 months
-        forecast = fit_model.forecast(steps=4)
-        forecast_dates = pd.date_range(start=data.index[-1], periods=5, freq='M')[1:]
-
-        return jsonify({
-            'index': forecast_dates.strftime('%Y-%m-%d').tolist(),
-            'values': forecast.tolist()
-        })
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
