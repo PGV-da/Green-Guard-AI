aa64635e387c5c4706e0fad4bd078dbf1fee2d9e
Vikram <vikramji283@gmail.com>
2025-04-23 22:17:42 +0530

    groq api from hugging face

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
     DATABASE = os.getenv('DATABASE_URL', 'main.db')
@@ -14,5 +13,6 @@ class Config:
     # Commodity Price API for data.gov.in
     API_KEY = "579b464db66ec23bdd0000012a392a566362485c4e95c88c71108314"
     BASE_URL = "https://api.data.gov.in/resource/35985678-0d79-46b4-9ed6-6f13308a1d24"
+    GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 
 config = Config()
